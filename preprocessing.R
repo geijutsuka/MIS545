3e2bafb39c460468909a200331888cd15fef501d
Rebecca Macaulay <macaulay@arizona.edu>
2020-08-09 11:30:22 -0700

    Cleaned up and commented preprocessing and added transformation file

--- preprocessing.R
+++ preprocessing.R
@@ -1,33 +1,44 @@
+#import data and set NA for blanks and nulls:
 shelterOutcomes <- read.csv('aac_shelter_outcomes.csv', stringsAsFactors = TRUE, header = TRUE, na.strings=c(""," ","NA"))
 nrow(shelterOutcomes)
-nrow(shelterOutcomes[complete.cases(shelterOutcomes),])
 head(shelterOutcomes)
-#mark Rto-adopt as an adoption
-shelterOutcomes$outcome_type[shelterOutcomes$outcome_type == "Rto-Adopt"] <- "Adoption"
+
+#See complete cases:
+#nrow(shelterOutcomes[complete.cases(shelterOutcomes),])
+
+#Simplify based on outcome_type data
 summary(shelterOutcomes$outcome_type)
-#remove rows with "return to owner" in outcome_type
-noReturns <- subset(shelterOutcomes, outcome_type != "Return to Owner")
-nrow(noReturns)
-#remove rows with "Unknown" for sex_upon_outcome
-noSexorReturn <- subset(noReturns, sex_upon_outcome != "Unknown")
-nrow(noSexorReturn)
-summary(noSexorReturn)
-#remove the columns monthyear and animal_id
-filteredOutcomes <- subset(noSexorReturn, select = -c(monthyear, animal_id))
+#Mark Rto-Adopt as an Adoption
+shelterOutcomes$outcome_type[shelterOutcomes$outcome_type == "Rto-Adopt"] <- "Adoption"
+#Check to see if Rto-Adopt values are gone 
+summary(shelterOutcomes$outcome_type)
+#Remove rows with "return to owner" and "NA" in outcome_type
+shelterOutcomes <- subset(shelterOutcomes, outcome_type != "Return to Owner")
+shelterOutcomes <- subset(shelterOutcomes, outcome_type != "NA")
+summary(shelterOutcomes$outcome_type)
+
+#Remove rows with "Unknown" and NULL in sex_upon_outcome
+shelterOutcomes <- subset(shelterOutcomes, sex_upon_outcome != "Unknown")
+shelterOutcomes <- subset(shelterOutcomes, sex_upon_outcome != "NULL")
+summary(shelterOutcomes$sex_upon_outcome)
+
+#Make sure removed data is minimal (less than 50% of 78256)
+nrow(shelterOutcomes)
+
+#Remove the unneeded columns monthyear, animal_id, age_upon_outcome (age will be calculated later)
+filteredOutcomes <- subset(shelterOutcomes, select = -c(monthyear, animal_id, age_upon_outcome, outcome_subtype))
 head(filteredOutcomes)
 
+#Calculate the age from the difference of datetime of the outcome and DOB and format the dates
+filteredOutcomes$age <- as.Date(as.character(filteredOutcomes$datetime), format="%Y-%m-%d")-
+                        as.Date(as.character(filteredOutcomes$date_of_birth), format="%Y-%m-%d")
+filteredOutcomes$datetime <- as.Date(as.character(filteredOutcomes$datetime), format="%Y-%m-%d")
+filteredOutcomes$date_of_birth <- as.Date(as.character(filteredOutcomes$date_of_birth), format="%Y-%m-%d")
+head(filteredOutcomes)
 
-narrowedOutcomes <- subset(filteredOutcomes, select = c('animal_type','breed','color', 'date_of_birth', 'datetime', 'sex_upon_outcome','outcome_type'))
-head(narrowedOutcomes)
+#Reorganize the columns to move age and sex from the end
+filteredOutcomes <- filteredOutcomes[c(1,2,3,4,5,6,7,8)]
 
-narrowedOutcomes$age <- as.Date(as.character(narrowedOutcomes$datetime), format="%Y-%m-%d")-
-                      as.Date(as.character(narrowedOutcomes$date_of_birth), format="%Y-%m-%d")
+head(filteredOutcomes)
 
-narrowedOutcomes <- narrowedOutcomes[c(1,2,3,4,5,8,6,7)]
-
-narrowedOutcomes$datetime <- as.Date(as.character(narrowedOutcomes$datetime), format="%Y-%m-%d")
-narrowedOutcomes$date_of_birth <- as.Date(as.character(narrowedOutcomes$date_of_birth), format="%Y-%m-%d")
-
-head(narrowedOutcomes)
-narrowedOutcomes
 
---
+++ transformation.R
@@ -0,0 +1,3 @@
+#Run preprocessing.R before running this file
+head(filteredOutcomes)
+

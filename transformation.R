60f6105a5c82cb6dab70f3161ba527e9d9304591
Rebecca Macaulay <macaulay@arizona.edu>
2020-08-09 12:29:48 -0700

    Make outcome_type into adoption vs. notadopted

--- transformation.R
+++ transformation.R
@@ -10,3 +10,12 @@ filteredOutcomes$name[is.na(filteredOutcomes$name)] <- "None"
 #Turn back into a factor and check summary
 filteredOutcomes$name <- as.factor(filteredOutcomes$name)
 summary(filteredOutcomes$name)
+
+#Format outcome_type to either be "adopted" or "notadopted"
+class(filteredOutcomes$outcome_type)
+filteredOutcomes$outcome_type <- as.character(filteredOutcomes$outcome_type)
+filteredOutcomes$outcome_type[filteredOutcomes$outcome_type != "Adoption"] <- "notAdopted"
+
+filteredOutcomes$outcome_type <- as.factor(filteredOutcomes$outcome_type)
+levels(filteredOutcomes$outcome_type)
+head(filteredOutcomes)

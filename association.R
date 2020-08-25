67f6b0b06cec71698f906ad074ce2913d4d91a42
Rebecca Macaulay <macaulay@arizona.edu>
2020-08-25 16:46:27 -0700

    Changed the support and confidence parameters and chose support for plots

--- association.R
+++ association.R
@@ -15,36 +15,38 @@ head(filteredOutcomes)
 categoricalOutcomes <- subset(filteredOutcomes, select = c('animal_type','age_range','name','sex_upon_outcome','outcome_type'), stringsAsFactors = TRUE)
 head(categoricalOutcomes)
 
-assoc_result <- apriori(categoricalOutcomes, parameter = list(sup=0.01, conf=0.5, target = "rules"),
+assoc_result <- apriori(categoricalOutcomes, parameter = list(sup=0.1, conf=0.5, target = "rules", minlen = 2),
                       appearance = list(default = 'lhs', rhs = c('outcome_type=Adoption', 'outcome_type=notAdopted'))
 )
 
 assoc_result
-# Output: 142 rules
+# Output: 37 rules
+plot(assoc_result, shading="lift", control=list(main = "Two-key plot"))
+
 inspect(assoc_result[1:5])
 top_five_support <- sort(assoc_result, decreasing = TRUE, by = "support")[1:5]
+inspect(top_five_support)
 top_five_confidence <- sort(assoc_result, decreasing = TRUE, by = "confidence")[1:5]
+inspect(top_five_confidence)
 plot(top_five_support, shading="lift", control=list(main = "Two-key plot sorted by support"))
 plot(top_five_confidence, shading="lift", control=list(main = "Two-key plot sorted by confidence"))
 
 # Adoption result
-adoptResult <- apriori(categoricalOutcomes, parameter = list(sup=0.01, conf=0.5, target = "rules"),
+adoptResult <- apriori(categoricalOutcomes, parameter = list(sup=0.1, conf=0.5, target = "rules"),
                      appearance = list(default = 'lhs', rhs = 'outcome_type=Adoption'))
 adoptResult <- sort(adoptResult, decreasing = TRUE, by = "confidence")
 inspect(adoptResult[1:5])
 
 # No adoption result
-noadoptResult <- apriori(categoricalOutcomes, parameter = list(sup=0.005, conf=0.5, target = "rules"), 
+noadoptResult <- apriori(categoricalOutcomes, parameter = list(sup=0.1, conf=0.5, target = "rules"), 
                      appearance = list(default = 'lhs', rhs = 'outcome_type=notAdopted'))
 noadoptResult <- sort(noadoptResult, decreasing = TRUE, by = "confidence")
 inspect(noadoptResult[1:5])
 
 # Back to top 5 results
 plot(top_five_support, method = "paracoord", shading = "support", control=list(main = "Parallel coordinates for top five support rules"))
-plot(top_five_confidence, method = "paracoord", shading = "confidence",  control=list(main = "Parallel coordinates for top five confidence rules"))
 
-ig_sup <- plot(top_five_support, method = "graph")
-ig_conf <- plot(top_five_confidence, method = "graph")
+ig_sup <- plot(top_five_support, method = "graph", control=list(main = "Graph for top five support rules"))
 # Data sorted by support is cleaner graph
 
 ig_sup_df <- get.data.frame(ig_sup, what = "both")
